4bbd5dfc5f6c54944b31e82323e45eedf7f861d7
amina dicko <aminadicko@gmail.com>
2026-03-03 20:25:31 +0000

    Initialisation de notre projet Amina_Design

---
+++ script.js
@@ -0,0 +1,18 @@
+//...
+const nouveauContact = {
+    nomClient:document.getElementById('nomClient'). value,
+    titreprojet:getElementById('titreProjet'). value,
+    numéroContact:getElementById('numéroContact'). value,
+    detailProjet:getElementById('detailProjet'). value
+
+} ;
+//...
+function afficherContact() { 
+    //...
+    liste . innerHTML+=`<li${c .nomClient} - ${c . titreprojet} - ${c.numeroContact} - ${c .detailProjet}
+                        <button onclick="updateContact(${index}) "Modifier </button> 
+                        <button onclick="updateContact(${index}) "Supprimer </button></li`
+    //...
+
+    `
+}
